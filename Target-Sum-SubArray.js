3f1a1944ecf3cfc4db5b577887fbc5cb1e4c970c
vinekmaurya2329 <vinekmaurya2329@gmail.com>
2023-09-25 12:04:46 +0530

    file added

---
+++ Target-Sum-SubArray.js
@@ -0,0 +1,24 @@
+//  target sum subarray --_--_--_--_--_--_--_
+
+let arr = [3,6,1,3,2,4,9];
+let sp = 0;
+let ep = 0;
+let csum =0;
+let target = 12;
+function targetSum(arr,target){
+
+while(ep<arr.length){
+ if(csum == target){
+     return [sp,ep-1]
+ }else if(csum < target){
+    csum+=arr[ep];
+    ep++;
+ }else{
+    csum-=arr[sp];
+    sp++;
+ }
+
+}
+return 'element does not exist';
+}
+console.log(targetSum(arr,target))
