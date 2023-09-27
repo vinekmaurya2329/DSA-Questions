ac2c341b34375a97b760d175a58e828d9a76d73a
vinekmaurya2329 <vinekmaurya2329@gmail.com>
2023-09-27 09:39:01 +0530

    file updated

--- Two-Sum.js
+++ Two-Sum.js
@@ -15,3 +15,28 @@ return [-1,-1]
 }
 console.log(twoSum(arr,target))
 
+
+
+// Second method --- - -- - -- - - -- - - -
+
+// let arr = [3,5,2,1,3,8,9,7];
+ arr = arr.sort();
+//  let target = 11;
+//  arr = arr.reverse();
+//  console.log(arr,target)
+function Twosum(arr,target){
+    let i =0;
+    let j = arr.length-1;
+while (i<j){
+    if(arr[i]+arr[j]==target) return [i,j]
+    else if(arr[i]+arr[j]>target){
+        j--;
+    }else{
+        i++;
+    }
+
+}
+return [-1,-1]
+}
+
+console.log(Twosum(arr,11))
